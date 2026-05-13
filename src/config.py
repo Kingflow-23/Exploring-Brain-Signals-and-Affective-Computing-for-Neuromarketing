ed729d4d0a05f7a09b77a7a33efb4df4c9e94762
Florian HOUNKPATIN <151558004+Kingflow-23@users.noreply.github.com>
2026-05-13 13:16:56 +0200

    Reorganize config.py for improved clarity and structure

--- src/config.py
+++ src/config.py
@@ -2,8 +2,18 @@
 DATASET_DIR = "data/"
 MODEL_DIR = "model/"
 
+# SEED Dataset
 N_TRIALS = 15
 
 LABEL_FILE = "label.mat"
-
 EEG_KEY_PATTERN = "_eeg"
+
+# Preprocessing
+WINDOW_SIZE = 400
+STEP_SIZE = 200
+
+# LLM CONFIGURATION
+DEFAULT_FS = 200
+DEFAULT_MODEL = "local-model"
+
+ALLOWED_LABELS = {"positive", "neutral", "negative"}
